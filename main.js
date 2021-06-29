2fbeecb7b6b2203396315938e0ac642efe46e5ba
ReyanshM <72616867+ReyanshM@users.noreply.github.com>
2021-06-29 20:13:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+song="";
+// function preload(){
+//     song=loadSound("Music.mp3");
+// }
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(500,500);
+    video.parent('liveVideo');
+}
+function Play(){
+    console.log("Hello");
+    // console.log(song);
+    // getAudioContext().resume();
+    // song.play();
+}
